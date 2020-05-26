c6a7d4bbc3ae337a35c83d2fdc4ec73dd5a78105
Glukhachev Sergey <ka_be@bk.ru>
2020-05-26 20:12:09 +0700

    attempt to solve

--- medium8-1-4.cs
+++ medium8-1-4.cs
@@ -1,25 +1,106 @@
-class Player
+using System;
+
+namespace Task
 {
-    public string Name { get; private set; }
-    public float WeaponCooldown { get; private set; }
-    public float MovementDirectionX { get; private set; }
-    public float MovementDirectionY { get; private set; }
-    public int Age { get; private set; }
-    public int WeaponDamage { get; private set; }
-    public float MovementSpeed { get; private set; }
-
-    public void Move()
+    internal class Program
     {
-        //Do move
+        public static void Main(string[] args)
+        {
+            var player = new Player("Mol`", 144128)
+            {
+                Mover = new ItemMover(144, 128, 20),
+                Attacker = new ItemAttacker(36, uint.MaxValue),
+            };
+        }
     }
 
-    public void Attack()
+    internal class Player : AbstractItem, IMover, IAttacker
     {
-        //attack
+        public Player(string name, int age) : base(name, age)
+        {
+        }
+
+        private ItemMover _mover;
+
+        public ItemMover Mover
+        {
+            get => _mover;
+            set => _mover = _mover == null ? value : _mover;
+        }
+
+        private ItemAttacker _attacker;
+
+        public ItemAttacker Attacker
+        {
+            get => _attacker;
+            set => _attacker = _attacker == null ? value : _attacker;
+        }
+
+        public bool IsReloading => throw new NotImplementedException();
+
+        public void Attack()
+        {
+            if (_attacker != null && !IsReloading)
+                throw new NotImplementedException();
+        }
+
+        public void Move(float x, float y)
+        {
+            if (_mover != null && !IsReloading)
+                throw new NotImplementedException();
+        }
     }
 
-    public bool IsReloading()
+    internal abstract class AbstractItem
     {
-        throw new NotImplementedException();
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        protected AbstractItem(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    internal interface IMover
+    {
+        ItemMover Mover { get; set; }
+
+        void Move(float x, float y);
+    }
+
+    internal interface IAttacker
+    {
+        ItemAttacker Attacker { get; set; }
+        bool IsReloading { get; }
+
+        void Attack();
+    }
+
+    internal class ItemMover
+    {
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+        public float MovementSpeed { get; private set; }
+
+        public ItemMover(float movementDirectionX, float movementDirectionY, uint movementSpeed)
+        {
+            MovementDirectionX = movementDirectionX;
+            MovementDirectionY = movementDirectionY;
+            MovementSpeed = movementSpeed;
+        }
+    }
+
+    internal class ItemAttacker
+    {
+        public float WeaponCooldown { get; private set; }
+        public int WeaponDamage { get; private set; }
+
+        public ItemAttacker(uint weaponCooldown, uint weaponDamage)
+        {
+            WeaponCooldown = weaponCooldown;
+            WeaponDamage = (int)weaponDamage;
+        }
     }
 }
